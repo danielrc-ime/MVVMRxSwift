e72b5317f1f16e87a64f084acc458d6f060eae47
Daniel Rodriguez <daniel.rodriguez@yonusa.com>
2022-01-19 16:03:13 -0600

    init

---
+++ MVVMRXSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMRXSwift
+//
+//  Created by Yonusa iOS on 19/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
